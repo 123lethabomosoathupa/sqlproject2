624676c42f3a0b5cf13cdb21cc188b53f0272dfb
123lethabomosoathupa <99099883+123lethabomosoathupa@users.noreply.github.com>
2022-04-23 10:39:12 +0200

    Final Product

--- datingapp.sql
+++ datingapp.sql
@@ -15,12 +15,34 @@ status_id integer references status (status_id)
 
 select * from my_contact;
 
-insert into my_contact(last_name, first_name, phone, email, gender, birthday, prof_id, zip_code, status_id)
-values ('van den Heever', 'Dyllan', '0678940134', 'dvanheever@gmail.com', 'Male', '2000-09-05', 4, 3, 3);
+drop my_contact cascade;
 
+insert into my_contact(last_name, first_name, phone, email, gender, birthday, prof_id, zip_code, status_id)
+values ('Walker', 'Johnny', '0810911348', 'jwalker@gmail.com', 'Male', '1996-07-06', 5, 4, 2),
+('Johanes', 'Jamey', '0673211354', 'msam@gmail.com', 'Female', '2001-03-12', 2, 4, 1);
+
+select my_contact.last_name, my_contact.first_name, my_contact.prof_id, profession.prof_id, profession.profession_name
+
+from my_contact
+left join profession
+on my_contact.prof_id = profession.prof_id;
+
+select my_contact.contact_id, my_contact.last_name, my_contact.first_name, my_contact.prof_id, profession.prof_id, profession.profession_name, my_contact.contact_id, contact_interests.contact_id, contact_interests.interest_id, interests.interest_id, interests.interest, contact_seeking.contact_id, contact_seeking.seeking_id, seeking.seeking_id, seeking.seeking_status 
+
+from my_contact
+left join profession
+on my_contact.prof_id = profession.prof_id
+left join contact_interests
+on my_contact.contact_id = contact_interests.contact_id
+left join interests
+on contact_interests.interest_id = interests.interest_id
+left join contact_seeking
+on my_contact.contact_id = contact_seeking.contact_id
+left join seeking
+on contact_seeking.seeking_id = seeking.seeking_id;
 
 create table profession(
-prof_id bigserial constraint prof_key primary key,
+prof_id bigserial constraint prof_key unique,
 profession_name varchar(50)
 
 );
@@ -119,10 +141,45 @@ contact_id integer references my_contact(contact_id)
 	
 );
 
+insert into contact_seeking(contact_id, seeking_id)
+values (1, 2),
+		(2, 3),
+		(3, 2),
+		(4, 2),
+		(5, 3),
+		(6, 1),
+		(7, 1),
+		(8, 2),
+		(9, 1),
+		(10, 3),
+		(11, 2),
+		(12, 1),
+		(13, 2),
+		(14, 3),
+		(15, 2);
+
 create table contact_interests(
 
 interest_id integer references interests(interest_id),
 contact_id integer references my_contact(contact_id)
 	
 );
+
+insert into contact_interests(contact_id, interest_id)
+values (1, 1),
+	(2, 3),
+	(3, 4),
+	(4, 7),
+	(5, 7),
+	(6, 6),
+	(7, 2 ),
+	(8, 3),
+	(9, 4),
+	(10, 7),
+	(11, 7),
+	(12, 6),
+	(13, 7),
+	(14, 7),
+	(15, 6);
+	
 
